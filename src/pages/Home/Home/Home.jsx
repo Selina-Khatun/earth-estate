0dd642c8ae006ea61bf7810acef7c4603e50a585
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2024-04-16 16:23:08 +0600

    added navbar and router

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+
+import Navbar from '../../../components/Navbar/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
